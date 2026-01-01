6315e321df3801c2199cbfbde7d499d7a3b11f19
Zebony919 <zebony9@gmail.com>
2026-01-02 12:20:12 +1300

    Add color choice

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-   console.log("Script loaded!");
-   const gridContainer = document.querySelector("#gridContainer");
-   console.log("Container found:", gridContainer);
+const gridContainer = document.querySelector("#gridContainer");
 
-   for (let i = 0; i < 256; i++) {
-       const gridSquare = document.createElement("div");
-       gridSquare.classList.add("gridSquare");
-       gridContainer.appendChild(gridSquare);
-   }
-   console.log("Squares created!");
+for (let i = 0; i < 256; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    gridContainer.appendChild(gridSquare);
+}
+
